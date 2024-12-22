ddc8440ff35cf68b2008e119330236ac167ee483
Alex Ur <ru.booking@yandex.ru>
2024-12-22 17:33:19 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@
 </template>
 
 <style lang="scss">
+body,
+html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
